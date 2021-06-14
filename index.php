f1d5235f66a1585043294c273b1e97019a9d69d7
Mathéo <matheo.galu56@gmail.com>
2021-06-14 10:29:26 +0200

    Restart from scratch

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include_once 'header.php'; ?>
+        <div class="wrapper">
+            <section class="index-intro">
+                <h1>This is an introduction</h1>
+                <p>And this is a paragraph</p>
+            </section>
+
+            <section class="index-categories">
+                <h2>Some Basic Category</h2>
+                <div class="index-categories-list">
+                    <div>
+                        <h3>Fun Stuff</h3>
+                    </div>
+                    <div>
+                        <h3>Serious Stuff</h3>
+                    </div>
+                    <div>
+                        <h3>Exiting Stuff</h3>
+                    </div>
+                    <div>
+                        <h3>Boring Stuff</h3>
+                    </div>
+                </div>
+            </section>
+        </div>
+    <?php include_once 'footer.php'; ?>
--- public/index.php
+++
@@ -1,4 +0,0 @@
-<?php 
-
-$app = require_once __DIR__ . '/../app/bootstrap.php';
-$app->run();
