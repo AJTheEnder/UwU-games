4161b3e81f9c1b9be4c3d23992f62f0cc331e6e4
Mathéo <matheo.galu56@gmail.com>
2021-06-22 12:56:07 +0200

    Clean file & added copyrights

--- model/gameModel.php
+++ model/gameModel.php
@@ -1,4 +1,14 @@
+<!--
+    * Created on Tue Jun 22 2021
+    *
+    * Copyright (c) 2021 - Mathéo G & Alex J & Jame FLC - All Right Reserved
+    *
+    * Licensed under the Apache License, Version 2.0
+    * Available on GitHub at https://github.com/Paracetamol56/UwU-game
+ -->
+
 <?php
+
     $sql = "SELECT gamesId, gamesName, gamesDescription, gamesDate, gamesDownloads, gamesLink, gamesCreator FROM games WHERE gamesId = :id";
     
     $sth = $dbh->prepare($sql);
--- view/headerView.php
+++ view/headerView.php
@@ -1,4 +1,13 @@
-<!DOCTYPE html>
+<!--
+    * Created on Tue Jun 22 2021
+    *
+    * Copyright (c) 2021 - Mathéo G & Alex J & Jame FLC - All Right Reserved
+    *
+    * Licensed under the Apache License, Version 2.0
+    * Available on GitHub at https://github.com/Paracetamol56/UwU-game
+ -->
+
+ <!DOCTYPE html>
 <html lang="fr" dir="ltr">
     <head>
         <meta charset="utf-8">
